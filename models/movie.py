949dbe9a6153ae352192157957ec783834159f25
Benhackettdelaney <benhackettdelaney9723@gmail.com>
2025-02-28 13:40:14 +0000

    fixing delete, all, and single routes for movies

--- models/movie.py
+++ models/movie.py
@@ -4,7 +4,7 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     movie_title = db.Column(db.String(100), nullable=False)
     movie_genres = db.Column(db.String(100), nullable=False)
-    movie_ratings = db.Column(db.Float(), nullable=False)
+    movie_ratings = db.Column(db.Float(precision=2), nullable=False)
 
     def __repr__(self):
         return f"Movie('{self.movie_title}', '{self.movie_genres}', '{self.movie_ratings}')"
--- routes/movie_routes.py
+++ routes/movie_routes.py
@@ -4,7 +4,7 @@ from extensions import db
 
 movie_bp = Blueprint('movie_bp', __name__)
 
-@movie_bp.route('/movies', methods=['POST']) 
+@movie_bp.route('/create', methods=['POST'])  
 def create_movie():
     data = request.json
     new_movie = Movie(movie_title=data['movie_title'], movie_ratings=data['movie_ratings'], movie_genres=data['movie_genres'])
@@ -12,12 +12,27 @@ def create_movie():
     db.session.commit()
     return jsonify({'message': 'Movie created successfully'}), 201
 
-@movie_bp.route('/movies', methods=['GET'])
+@movie_bp.route('', methods=['GET'])  
 def get_movies():
     movies = Movie.query.all()
-    return jsonify([{"id": movies.id, "movie_title": movies.title, "movie_ratings": movies.ratings, "movie_genres": movies.genres} for movies in movies]), 200
+    return jsonify([{
+        "id": movie.id,
+        "movie_title": movie.movie_title,
+        "movie_ratings": movie.movie_ratings,
+        "movie_genres": movie.movie_genres
+    } for movie in movies]), 200
 
-@movie_bp.route('/movies/<int:id>', methods=['PUT'])
+@movie_bp.route('/<int:id>', methods=['GET'])  
+def single_movie(id):
+    movie = Movie.query.get_or_404(id)
+    return jsonify({
+        "id": movie.id,
+        "movie_title": movie.movie_title,
+        "movie_ratings": movie.movie_ratings,
+        "movie_genres": movie.movie_genres
+    }), 200
+
+@movie_bp.route('/update/<int:id>', methods=['PUT'])
 def update_movie(id):
     data = request.json
     movie = Movie.query.get_or_404(id)
@@ -27,9 +42,9 @@ def update_movie(id):
     db.session.commit()
     return jsonify({'message': 'Movie updated successfully'}), 200
 
-@movie_bp.route('/movies/<int:id>', methods=['DELETE'])
+@movie_bp.route('/delete/<int:id>', methods=['DELETE'])
 def delete_movie(id):
-    movie = Movie.query.get_or_404(id)  
+    movie = Movie.query.get_or_404(id)
     db.session.delete(movie)
     db.session.commit()
     return jsonify({'message': 'Movie deleted successfully'}), 200
